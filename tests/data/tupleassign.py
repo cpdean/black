2d0ae71a20382b1a4038745fbf29b387062360f3
Conrad Dean <conrad.p.dean@gmail.com>
2019-12-24 19:53:32 -0500

    fix a test

--- tests/data/tupleassign.py
+++ tests/data/tupleassign.py
@@ -11,10 +11,10 @@ this_will_be_wrapped_in_parens, = struct.unpack(b"12345678901234567890")
 
 # This is a standalone comment.
 (
-    sdfjklsdfsjldkflkjsf,
-    sdfjsdfjlksdljkfsdlkf,
-    sdfsdjfklsdfjlksdljkf,
-    sdsfsdfjskdflsfsdf,
+ sdfjklsdfsjldkflkjsf,
+ sdfjsdfjlksdljkfsdlkf,
+ sdfsdjfklsdfjlksdljkf,
+ sdsfsdfjskdflsfsdf,
 ) = (1, 2, 3)
 
 # This is as well.
